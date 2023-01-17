a6c808a9a47151619a752feb920d8c473190b9c0
Daniel Bennett <dnnybnt@gmail.com>
2023-01-17 14:17:36 +0100

    added simple country filter

--- src/dotnet/DBN.CountryInfo.Angular/ClientApp/src/app/country-info/country-info.component.ts
+++ src/dotnet/DBN.CountryInfo.Angular/ClientApp/src/app/country-info/country-info.component.ts
@@ -6,12 +6,30 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './country-info.component.html',
 })
 export class CountryInfoComponent {
+  private _countries: Country[] = [];
   public countries: Country[] = [];
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
     http.get<Country[]>(baseUrl + 'countryinfo').subscribe(result => {
-      this.countries = result;
+      this._countries = result;
+      this.filterCountries('');
     }, error => console.error(error));
+  }
+
+  filterCountries(searchText: string) {
+    searchText = searchText.trim().toLowerCase();
+    if (searchText) {
+      this.countries = this._countries.filter((obj) => {
+        return obj.code.toLowerCase().includes(searchText)
+          || obj.name.toLowerCase().includes(searchText)
+      });
+    } else {
+      this.countries = this._countries;
+    }
+  }
+
+  onFilterCountries(event: any) {
+    this.filterCountries(event.target.value.toLowerCase().trim());
   }
 }
 
